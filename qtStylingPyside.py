d510c79377ccf662371cc5f51666cded2fc93c6a
Jonas Avrin <jawabiscuit@users.noreply.github.com>
2021-04-08 16:08:07 -0400

    Style sheet widget customization

--- qtStylingPyside.py
+++ qtStylingPyside.py
@@ -2,6 +2,8 @@
 import os
 import sys
 
+from PySide2.QtCore import QResource
+
 from PySide2.QtWidgets import (
     QApplication,
     QWidget,
@@ -15,12 +17,12 @@ class DefaultWidget(QWidget):
     """Default widget with no style mods"""
 
     @classmethod
-    def init(cls, args):
+    def init(cls, *args):
         cls.initGlobalStyle(args)
         widget = cls()
         widget.setWindowTitle(cls.__name__)
         widget.initUi()
-        widget.initInstanceStyle(args)
+        widget.initInstanceStyle(*args)
         return widget
 
     @classmethod
@@ -30,7 +32,14 @@ class DefaultWidget(QWidget):
         """
         pass
 
-    def initInstanceStyle(self, args):
+    @classmethod
+    def getStyleSheet(cls):
+        """
+        Reads in the style sheet resource
+        """
+        raise NotImplementedError()
+
+    def initInstanceStyle(self, *args):
         """
         Initialize style that will be applied to this instance
         """
@@ -38,10 +47,10 @@ class DefaultWidget(QWidget):
 
     def initUi(self):
         self.setObjectName(self.__class__.__name__)
-        self.button = button = QPushButton("Button", self)
-        self.group = group = QGroupBox('Group', self)
+        button = QPushButton("Button", self)
+        group = QGroupBox('Group', self)
 
-        self.widgets = widgets = [
+        widgets = [
             button,
         ]
         innerLayout = QVBoxLayout(group)
@@ -53,17 +62,47 @@ class DefaultWidget(QWidget):
         layout.addWidget(group)
 
 
+class QssWidget(DefaultWidget):
+
+    @classmethod
+    def initGlobalStyle(cls, *args):
+        """
+        Initialize style that will be used across the application
+        """
+        QApplication.instance().setStyleSheet(cls.getStyleSheet())
+
+    @classmethod
+    def initInstanceStyle(cls, *args):
+        """
+        Initialize style that will be applied to this instance
+        """
+        pass
+
+    @classmethod
+    def getStyleSheet(cls):
+        """
+        Reads in the style sheet resource
+        """
+        dirname = os.path.dirname(__file__)
+        rccPath = os.path.join(dirname, "resources.rcc")
+        QResource.registerResource(rccPath)
+        qssPath = os.path.join(dirname, "sample.css")
+        with open(qssPath, "r") as fh:
+            return fh.read()
+
+
 if __name__ == "__main__":
     from collections import deque
 
     # Style to window type map
     styleMap = dict(
         default=DefaultWidget,
+        qss=QssWidget,
     )
     _app = QApplication(sys.argv)
 
     # Process the arguments
-    mode = "default"
+    mode = "qss"
     args = deque(sys.argv[1:])
     error = "Provide a styling mode from the list: [{}]".format(
         ", ".join(sorted(styleMap.keys())))
@@ -75,7 +114,6 @@ if __name__ == "__main__":
     uiType = styleMap.get(mode, False)
     if not uiType:
         raise RuntimeError(error)
-        sys.exit(1)
 
     ui = uiType.init(args)
     ui.show()
