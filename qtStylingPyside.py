6ff74b6097a4cc0afc668f8b21fa174450f64291
Jonas Avrin <jawabiscuit@users.noreply.github.com>
2021-04-09 10:54:31 -0400

    Palette widget implementation

--- qtStylingPyside.py
+++ qtStylingPyside.py
@@ -3,8 +3,15 @@ import os
 import sys
 
 from PySide2.QtCore import (
+    Qt,
     QResource,
     Signal,
+)
+
+from PySide2.QtGui import (
+    QPalette,
+    QColor,
+    QBrush,
 )
 
 from PySide2.QtWidgets import (
@@ -15,6 +22,7 @@ from PySide2.QtWidgets import (
     QVBoxLayout,
     QLineEdit,
     QComboBox,
+    QStyle,
 )
 
 
@@ -40,6 +48,8 @@ class DefaultWidget(QWidget):
         Initialize style that will be used across the application
         """
         QApplication.instance().setStyleSheet("")
+        QApplication.instance().setPalette(
+            QApplication.style().standardPalette())
 
     @classmethod
     def getStyleSheet(cls):
@@ -85,6 +95,37 @@ class DefaultWidget(QWidget):
         layout.addWidget(group)
 
 
+class PaletteWidget(DefaultWidget):
+    """
+    A widget that customizes its appearance by assigning a palette
+
+    .. note::
+
+        Palettes are NOT recommended. They are inconsistent, confusing and can
+        only customize color. See `Qt docs QPalette`_
+
+    .. _Qt docs QPalette: https://doc.qt.io/qt-5/qpalette.html
+    """
+
+    @classmethod
+    def initGlobalStyle(cls, *args):
+        """
+        Initialize style that will be used across the application
+        """
+        palette = QPalette()
+        palette.setColor(QPalette.WindowText, QColor(150, 150, 255))
+        palette.setBrush(QPalette.Window, QBrush(Qt.darkMagenta, Qt.Dense7Pattern))
+        palette.setColor(QPalette.Base, Qt.darkGreen)
+        palette.setColor(QPalette.AlternateBase, Qt.darkMagenta)
+        QApplication.instance().setPalette(palette)
+
+    def initInstanceStyle(self, *args):
+        """
+        Initialize style that will be applied to this instance
+        """
+        pass
+
+
 class QssWidget(DefaultWidget):
 
     @classmethod
@@ -93,6 +134,11 @@ class QssWidget(DefaultWidget):
         Initialize style that will be used across the application
         """
         QApplication.instance().setStyleSheet(cls.getStyleSheet())
+        # For the purposes of demo'ing If a palette has been applied previously
+        # as well as reset back to standard, simply running setStyleSheet() will
+        # unexpectedly re-apply palette settings!
+        QApplication.instance().setPalette(
+            QApplication.style().standardPalette())
 
     def initInstanceStyle(self, *args):
         """
@@ -138,6 +184,7 @@ class ControllerWidget(QWidget):
     _demoWidget = None
 
     widget_type_map = dict(default=DefaultWidget,
+                           palette=PaletteWidget,
                            qss=QssWidget,)
 
     def __init__(self):
@@ -148,31 +195,35 @@ class ControllerWidget(QWidget):
         widget.show()
 
         self.styleCombo = styleCombo = QComboBox()
-        setButton = QPushButton("&Set")
+        applyButton = QPushButton("&Apply")
         errorButton = QPushButton("Error")
         quitButton = QPushButton("&Quit")
 
-        setButton.setToolTip(
+        applyButton.setToolTip(
             "Apply the style selected in the drop-down")
         errorButton.setToolTip(
             "Apply styling to individual widgets indicating an error")
         quitButton.setToolTip(
             "Quit the application")
 
-        styleCombo.addItems(["default", "qss"])
+        styleCombo.addItems(sorted(self.widget_type_map.keys()))
 
         layout = QVBoxLayout(self)
         layout.addWidget(styleCombo)
-        layout.addWidget(setButton)
+        layout.addWidget(applyButton)
         layout.addWidget(errorButton)
         layout.addWidget(quitButton)
         layout.addStretch()
 
-        setButton.clicked.connect(self.onSetClicked)
+        applyButton.clicked.connect(self.onApplyClicked)
         errorButton.clicked.connect(self.onErrorClicked)
         quitButton.clicked.connect(self.close)
 
-    def onSetClicked(self):
+    def closeEvent(self, event):
+        ControllerWidget._demoWidget.deleteLater()
+        event.accept()
+
+    def onApplyClicked(self):
         widget = ControllerWidget._demoWidget
         uiSelection = self.styleCombo.currentText()
         uiType = self.widget_type_map.get(uiSelection)
@@ -180,10 +231,6 @@ class ControllerWidget(QWidget):
         new = ControllerWidget._demoWidget = uiType.init()
         new.show()
         widget.close()
-
-    def closeEvent(self, event):
-        ControllerWidget._demoWidget.deleteLater()
-        event.accept()
 
     def onErrorClicked(self):
         widget = ControllerWidget._demoWidget
